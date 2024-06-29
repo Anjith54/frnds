b0d966006310119b15fd966b11de8b08cdbacbed
Anjith54 <anjithachu54@gmail.com>
2024-06-29 14:13:46 +0530

    hg

---
+++ app/src/main/java/com/examplei/frndsapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.examplei.frndsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
